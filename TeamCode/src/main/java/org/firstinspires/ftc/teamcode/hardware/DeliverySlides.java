567ff7f228c8ce6ce10597c50a373c1d2ad0f792
Sean Krueger <skrueger2270@gmail.com>
2023-11-01 17:04:54 -0500

    Add slide extensions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/DeliverySlides.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.utils.controller.PID;
+import org.firstinspires.ftc.teamcode.utils.hardware.MotorBuilder;
+
+public class DeliverySlides {
+    public enum BackdropHeights {
+        FIRST_LEVEL(0),
+        SECOND_LEVEL(200),
+        THIRD_LEVEL(400),
+        FOURTH_LEVEL(600),
+        FIFTH_LEVEL(800),
+        SIXTH_LEVEL(1000);
+
+        public final int targetPosition;
+
+        BackdropHeights(int targetPosition) {
+            this.targetPosition = targetPosition;
+        }
+    }
+
+    private static final double lP = 0.0, lI = 0.0, lD = 0.0;
+    private static final double rP = 0.0, rI = 0.0, rD = 0.0;
+
+    private final PID leftPIDController, rightPIDController;
+    private final DcMotorEx leftExtension, rightExtension;
+
+    public DeliverySlides(HardwareMap hardwareMap) {
+        leftExtension = new MotorBuilder(hardwareMap, "left ext")
+                .setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+                .resetEncoder()
+                .build();
+        rightExtension = new MotorBuilder(hardwareMap, "right ext")
+                .setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+                .resetEncoder()
+                .setDirection(DcMotorSimple.Direction.REVERSE)
+                .build();
+
+        leftPIDController = new PID(0.0, leftExtension.getCurrentPosition(), lP, lI, lD);
+        rightPIDController = new PID(0.0, rightExtension.getCurrentPosition(), rP, rI, rD);
+    }
+
+    public void setHeight(BackdropHeights targetLevel) {
+        leftPIDController.setSetPoint(targetLevel.targetPosition);
+        rightPIDController.setSetPoint(targetLevel.targetPosition);
+    }
+
+    public void update() {
+        double leftNewPower = leftPIDController.update(leftExtension.getCurrentPosition());
+        double rightNewPower = rightPIDController.update(rightExtension.getCurrentPosition());
+
+        leftExtension.setPower(leftNewPower);
+        rightExtension.setPower(rightNewPower);
+    }
+}
