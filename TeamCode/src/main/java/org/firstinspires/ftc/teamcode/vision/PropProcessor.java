7e8561fb94e7f67aaf434fd07637794c287ff3ae
AadilBar <aadilbar2008@gmail.com>
2023-11-11 11:09:28 -0600

    webcame code work in progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/PropProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/PropProcessor.java
@@ -8,9 +8,11 @@ import org.firstinspires.ftc.robotcore.external.function.Continuation;
 import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import java.util.concurrent.atomic.AtomicReference;
@@ -40,6 +42,7 @@ public class PropProcessor implements VisionProcessor, CameraStreamSource {
 
     private double[] spikeLocations = new double[3];
     private int largestSpikeIndex = -1;
+    private Mat processMat= new Mat();
 
     @Override
     public void init(int width, int height, CameraCalibration calibration) {
@@ -48,12 +51,21 @@ public class PropProcessor implements VisionProcessor, CameraStreamSource {
 
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
-        Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGB2HSV);
-        Imgproc.GaussianBlur(frame, frame, BLUR_SIZE, 0);
+        Imgproc.cvtColor(frame, processMat, Imgproc.COLOR_RGB2HSV);
+        Imgproc.GaussianBlur(processMat, processMat, BLUR_SIZE, 0);
 
-        spikeLocations[0] = Core.mean(frame.submat(LEFT_SPIKE)).val[0];
-        spikeLocations[1] = Core.mean(frame.submat(MIDDLE_SPIKE)).val[0];
-        spikeLocations[2] = Core.mean(frame.submat(RIGHT_SPIKE)).val[0];
+
+        spikeLocations[0] = Core.mean(processMat.submat(LEFT_SPIKE)).val[0];
+        spikeLocations[1] = Core.mean(processMat.submat(MIDDLE_SPIKE)).val[0];
+        spikeLocations[2] = Core.mean(processMat.submat(RIGHT_SPIKE)).val[0];
+
+        Imgproc.rectangle(frame, LEFT_SPIKE, new Scalar(255, 0, 0), 5);
+        Imgproc.rectangle(frame, RIGHT_SPIKE, new Scalar(255, 0, 0), 5);
+        Imgproc.rectangle(frame, MIDDLE_SPIKE, new Scalar(255, 0, 0), 5);
+
+        Bitmap b = Bitmap.createBitmap(processMat.width(), processMat.height(), Bitmap.Config.RGB_565);
+        Utils.matToBitmap(frame, b);
+        lastFrame.set(b);
 
         return frame;
     }
