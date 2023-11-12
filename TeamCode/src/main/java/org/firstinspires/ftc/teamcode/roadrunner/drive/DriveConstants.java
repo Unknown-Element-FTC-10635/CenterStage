fcf9327e8be0b3becc365071faa117ebdaecc867
Sean Krueger <skrueger2270@gmail.com>
2023-11-12 17:10:30 -0600

    Straight test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -55,8 +55,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0125;
-    public static double kA = 0.005;
+    public static double kV = 0.01;
+    public static double kA = 0.004;
     public static double kStatic = 0;
 
     /*
@@ -87,8 +87,8 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
      
      */
-    public static double MAX_VEL = 25;
-    public static double MAX_ACCEL = 25;
+    public static double MAX_VEL = 45;
+    public static double MAX_ACCEL = 45;
     public static double MAX_ANG_VEL = Math.toRadians(90);
     public static double MAX_ANG_ACCEL = Math.toRadians(90);
 
