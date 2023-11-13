b1b0295a4c6f9aecf71079ad0c1ebd4f343886b6
AadilBar <aadilbar2008@gmail.com>
2023-11-12 19:00:26 -0600

    no strafe auto paths

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,10 +1,15 @@
 package com.example.meepmeeptesting;
 
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Vector2d;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -19,89 +24,102 @@ public class MeepMeepTesting {
                 // We set this bot to be blue
                 .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-
-        blueBotPos1.runAction(blueBotPos1.getDrive().actionBuilder(new Pose2d(15, 62, Math.toRadians(270)))
-                .splineTo(new Vector2d(9, 38.5), Math.toRadians(225))
-                .waitSeconds(2)
-                .setReversed(true)
-                .splineTo(new Vector2d(50, 41.5), Math.toRadians(0))
-                .lineToX(45)
-                .strafeTo(new Vector2d(45, 60))
-                .build());
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(15, 62, Math.toRadians(270)))
+                                .splineTo(new Vector2d(9, 38.5), Math.toRadians(225))
+                                .waitSeconds(2)
+                                .setReversed(true)
+                                .splineTo(new Vector2d(50, 41.5), Math.toRadians(0))
+                                .forward(15)
+                                .lineTo(new Vector2d(45,60))
+                                .build()
+                );
 
         RoadRunnerBotEntity blueBotPos2 = new DefaultBotBuilder(meepMeep)
                 .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60,60, Math.toRadians(180), Math.toRadians(105), 15)
-                .build();
-
-        blueBotPos2.runAction(blueBotPos2.getDrive().actionBuilder(new Pose2d(15, 62, Math.toRadians(270)))
-                .splineTo(new Vector2d(10, 35), Math.toRadians(270))
-                .waitSeconds(2)
-                .setReversed(true)
-                .splineTo(new Vector2d(50, 36), Math.toRadians(0))
-                .lineToX(45)
-                .strafeTo(new Vector2d(45, 60))
-                .build());
-
+                .followTrajectorySequence(drive ->
+                drive.trajectorySequenceBuilder(new Pose2d(15, 62, Math.toRadians(270)))
+                        .splineTo(new Vector2d(10, 35), Math.toRadians(270))
+                        .waitSeconds(2)
+                        .setReversed(true)
+                        .splineTo(new Vector2d(50, 36), Math.toRadians(0))
+                        .forward(15)
+                        .lineTo(new Vector2d(45,60))
+                        .build()
+        );
+//
         RoadRunnerBotEntity blueBotPos3 = new DefaultBotBuilder(meepMeep)
                 .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60,60, Math.toRadians(180), Math.toRadians(105), 15)
-                .build();
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(15, 62, Math.toRadians(270)))
+                                .splineTo(new Vector2d(25, 38.5), Math.toRadians(230))
+                                .waitSeconds(2)
+                                .setReversed(true)
+                                .splineTo(new Vector2d(50, 30), Math.toRadians(0))
+                                .forward(15)
+                                .lineTo(new Vector2d(45,60))
+                                .build()
+                );
 
-        blueBotPos3.runAction(blueBotPos3.getDrive().actionBuilder(new Pose2d(15, 62, Math.toRadians(270)))
-                .splineTo(new Vector2d(25, 38.5), Math.toRadians(230))
-                .waitSeconds(2)
-                .setReversed(true)
-                .splineTo(new Vector2d(50, 30), Math.toRadians(0))
-                .lineToX(45)
-                .strafeTo(new Vector2d(45, 60))
-                .build());
 
+//
         RoadRunnerBotEntity redBotPos1 = new DefaultBotBuilder(meepMeep)
                 // We set this bot to be red
                 .setColorScheme(new ColorSchemeRedDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(15, -62, Math.toRadians(90)))
+                            .splineTo(new Vector2d(9, -38.5), Math.toRadians(135))
+                            .waitSeconds(2)
+                            .setReversed(true)
+                            .splineTo(new Vector2d(50, -30), Math.toRadians(0))
+                            .forward(15)
+                            .lineTo(new Vector2d(45, -60))
+                            .build()
+                );
 
-        redBotPos1.runAction(redBotPos1.getDrive().actionBuilder(new Pose2d(15, -62, Math.toRadians(90)))
-                .splineTo(new Vector2d(9, -38.5), Math.toRadians(135))
-                .waitSeconds(2)
-                .setReversed(true)
-                .splineTo(new Vector2d(50, -30), Math.toRadians(0))
-                .lineToX(45)
-                .strafeTo(new Vector2d(45, -60))
-                .build());
 
         RoadRunnerBotEntity redBotPos2 = new DefaultBotBuilder(meepMeep)
                 .setColorScheme(new ColorSchemeRedDark())
                 .setConstraints(60,60, Math.toRadians(180), Math.toRadians(105), 15)
-                .build();
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(15, -62, Math.toRadians(90)))
+                                .splineTo(new Vector2d(10, -35), Math.toRadians(95))
+                                .waitSeconds(2)
+                                .setReversed(true)
+                                .splineTo(new Vector2d(50, -36), Math.toRadians(0))
+                                .forward(15)
+                                .lineTo(new Vector2d(45, -60))
+                                .build()
+                );
 
-        redBotPos2.runAction(redBotPos2.getDrive().actionBuilder(new Pose2d(15, -62, Math.toRadians(90)))
-                .splineTo(new Vector2d(10, -35), Math.toRadians(95))
-                .waitSeconds(2)
-                .setReversed(true)
-                .splineTo(new Vector2d(50, -36), Math.toRadians(0))
-                .lineToX(45)
-                .strafeTo(new Vector2d(45, -60))
-                .build());
-
+//
         RoadRunnerBotEntity redBotPos3 = new DefaultBotBuilder(meepMeep)
                 .setColorScheme(new ColorSchemeRedDark())
                 .setConstraints(60,60, Math.toRadians(180), Math.toRadians(105), 15)
-                .build();
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(15, -62, Math.toRadians(90)))
+                                .splineTo(new Vector2d(25, -38.5), Math.toRadians(135))
+                                .waitSeconds(2)
+                                .setReversed(true)
+                                .splineTo(new Vector2d(50, -41.5), Math.toRadians(0))
+                                .forward(15)
+                                .lineTo(new Vector2d(45, -60))
+                                .build()
+                );
 
-        redBotPos3.runAction(redBotPos3.getDrive().actionBuilder(new Pose2d(15, -62, Math.toRadians(90)))
-                .splineTo(new Vector2d(25, -38.5), Math.toRadians(135))
-                .waitSeconds(2)
-                .setReversed(true)
-                .splineTo(new Vector2d(50, -41.5), Math.toRadians(0))
-                .lineToX(45)
-                .strafeTo(new Vector2d(45, -60))
-                .build());
-        
-
+//        redBotPos3.runAction(redBotPos3.getDrive().actionBuilder(new Pose2d(15, -62, Math.toRadians(90)))
+//                .splineTo(new Vector2d(25, -38.5), Math.toRadians(135))
+//                .waitSeconds(2)
+//                .setReversed(true)
+//                .splineTo(new Vector2d(50, -41.5), Math.toRadians(0))
+//                .lineToX(45)
+//                .strafeTo(new Vector2d(45, -60))
+//                .build());
+//
+//
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
@@ -112,10 +130,10 @@ public class MeepMeepTesting {
                 // .addEntity(blueBotPos2)
                 // .addEntity(blueBotPos1)
                 // .addEntity(blueBotPos2)
-                .addEntity(blueBotPos3)
+                .addEntity(redBotPos3)
                 // .addEntity(redBotPos1)
                 // .addEntity(redBotPos2)
-                .addEntity(redBotPos3)
+//                .addEntity(redBotPos3)
                 .start();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/BlueLeft20.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class BlueLeft20 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
