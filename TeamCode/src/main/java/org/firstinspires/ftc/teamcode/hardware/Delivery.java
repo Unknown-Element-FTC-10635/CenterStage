d6bff8fa3ae3032d4365af726ee495daabacdb1d
Sean Krueger <skrueger2270@gmail.com>
2023-12-11 20:50:49 -0600

    Remove servo that no longer exists

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Delivery.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Delivery.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Delivery {
     public enum DeliveryState {
-        INTAKE_PICKUP(0.19, 0.19, 0.65),
+        INTAKE_PICKUP(0.175, 0.175, 0.65),
         INTAKE_HOLD(0.5, 0.5, 0.65),
         TRANSITION_2(0.7, 0.7, 0.0),
         SCORE(0.925, 0.925, 0.00),
@@ -13,25 +13,21 @@ public class Delivery {
         UNKNOWN(0.6, 0.6, 0.0);
 
         public final double pitchPositionRight;
-        public final double pitchPositionLeft;
         public final double rollPosition;
 
         DeliveryState(double pitchServoRight, double pitchServoLeft, double rollServo) {
             this.pitchPositionRight = pitchServoRight;
-            this.pitchPositionLeft = pitchServoLeft;
             this.rollPosition = rollServo;
         }
     }
 
     private final Servo pitchServoRight;
-    private final Servo pitchServoLeft;
     private final Servo rollServo;
 
     private DeliveryState deliveryState;
 
     public Delivery(HardwareMap hardwareMap) {
         pitchServoRight = hardwareMap.get(Servo.class, "pitch right");
-        pitchServoLeft = hardwareMap.get(Servo.class, "pitch left");
         rollServo = hardwareMap.get(Servo.class, "roll");
 
         deliveryState = DeliveryState.UNKNOWN;
@@ -39,7 +35,6 @@ public class Delivery {
 
     public void setDeliveryState(DeliveryState deliveryState) {
         this.deliveryState = deliveryState;
-        pitchServoLeft.setPosition(deliveryState.pitchPositionLeft);
         pitchServoRight.setPosition(deliveryState.pitchPositionRight);
         rollServo.setPosition(deliveryState.rollPosition);
     }
@@ -54,10 +49,6 @@ public class Delivery {
 
     public double getRollPosition() {
         return rollServo.getPosition();
-    }
-
-    public double getLeftRotationPosition() {
-        return pitchServoLeft.getPosition();
     }
 
     public double getRightRotationPosition() {
